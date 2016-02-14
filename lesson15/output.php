c198f76c84e243fc9a7f7a82191005b4d8cffa6f
BJIAST <bjiast.01@gmail.com>
2016-02-14 21:23:07 +0200

    finish less15

--- lesson15/output.php
+++ lesson15/output.php
@@ -1,4 +1,30 @@
 <?php
- echo "Hello world!";
 
+$login = $_POST['login'];
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$sex = $_POST['sex'];
+
+$dateNow = date(Y);
+$birthday = $_POST['age'];
+$level = $dateNow-$birthday;
+
+if ($sex = 1){
+    $sex = "Мужчина";
+}else {
+    $sex[2] = "Женщина";
+}
+?>
+
+<?php
+if (isset($_POST["name"])) {
+    echo "<h3>Ваши данные:</h3>";
+    echo "<pre>";
+    echo "Здравствуйте, " . "<span id='surname'>" .$surname . "</span> " . "<span id='name'>". $name ."</span>.";
+    echo "<br>Ваш логин: <span id='login'>" . $login . "</span>";
+    echo "<br>" . $sex ." возраста ". $level . " лет, верно?";
+    echo "</pre>";
+    echo "<a id='welldone' class='btn btn-primary link'>Верно</a>";
+    echo "<a class='link' id='link-wrong'>Что-то пошло не так</a>";
+}
 ?>
